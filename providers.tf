74846be9c80922ae344fe57abea02e2873601e85
jtfernandes <jtfernandes.it@gmail.com>
2022-09-12 10:58:31 -0300

    update version

--- providers.tf
+++ providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     google = {
       source = "hashicorp/google"
-      version = "4.22.0"
+      version = "4.34.0"
     }
   }
 }
@@ -10,9 +10,9 @@ terraform {
 # Configura o Provider Google Cloud com o Projeto
 provider "google" {
 
-  credentials = file("/home/vagrant/gcpkey.json")
+  credentials = file("/home/jtfernandes_it/gcpkey.json")
 
-  project = "teste-351601"
+  project = "lab-devops-fiap-360819"
   region  = "us-west1"
   zone    = "us-west1-b"
 }
