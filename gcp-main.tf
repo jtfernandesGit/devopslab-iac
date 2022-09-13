490a88c5d5cc12f7ac7ba3c019acba590468ed9f
jtfernandes <jtfernandes.it@gmail.com>
2022-09-13 10:40:49 -0300

    fix app engine

--- gcp-main.tf
+++ gcp-main.tf
@@ -1,7 +1,7 @@
 #update
 resource "google_app_engine_application" "app" {
   project = "lab-devops-fiap-360819"
-  location_id = "us-central1"
+  location_id = "us-central"
 }
 
 resource "google_artifact_registry_repository" "my-repo" {
